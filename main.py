0b3733fea8cd27da370868f3db51239cffb7b764
Leon van Wissen <leon@vwissen.nl>
2020-01-27 19:22:03 +0100

    to rdf

--- main.py
+++ main.py
@@ -6,31 +6,113 @@ import re
 import requests
 from bs4 import BeautifulSoup
 
+from rdflib import Graph, URIRef, Literal, XSD, Namespace, RDFS, BNode, OWL
+from rdfalchemy import rdfSubject, rdfMultiple, rdfSingle
+
 PORTRETURL = "http://www.schrijverskabinet.nl/schrijverskabinet/"
 
+schema = Namespace("https://schema.org/")
+bio = Namespace("http://purl.org/vocab/bio/0.1/")
 
-def main():
+ns = Namespace(
+    "https://data.create.humanities.uva.nl/id/datasets/schrijverskabinet/")
 
-    DATA = {
-        'portrets': {},
-        'metadata': {
-            'date': datetime.datetime.now().isoformat()
+
+class Entity(rdfSubject):
+    rdf_type = URIRef('urn:entity')
+
+    label = rdfMultiple(RDFS.label)
+    name = rdfMultiple(schema.name)
+
+    mainEntityOfPage = rdfSingle(schema.mainEntityOfPage)
+    sameAs = rdfMultiple(OWL.sameAs)
+
+
+class CreativeWork(Entity):
+    rdf_type = schema.CreativeWork
+
+    publication = rdfMultiple(schema.publication)
+    author = rdfMultiple(schema.author)
+
+    mainEntity = rdfSingle(schema.mainEntity)
+    about = rdfSingle(schema.about)
+
+
+class ScholarlyArticle(CreativeWork):
+    rdf_type = schema.ScholarlyArticle
+
+
+class VisualArtwork(CreativeWork):
+    rdf_type = schema.VisualArtwork
+
+    artist = rdfSingle(schema.artist)
+
+
+class PublicationEvent(Entity):
+    rdf_type = schema.PublicationEvent
+
+    startDate = rdfSingle(schema.startDate)
+    location = rdfSingle(schema.location)
+
+    publishedBy = rdfMultiple(schema.publishedBy)
+
+
+class Place(Entity):
+    rdf_type = schema.Place
+
+
+class Marriage(Entity):
+    rdf_type = bio.Marriage
+
+    date = rdfSingle(bio.date)
+    partner = rdfMultiple(bio.partner)
+    place = rdfSingle(bio.place)
+
+    subjectOf = rdfMultiple(schema.subjectOf)
+
+
+class Person(Entity):
+    rdf_type = schema.Person
+
+    birthPlace = rdfSingle(schema.birthPlace)
+    deathPlace = rdfSingle(schema.deathPlace)
+
+    birthDate = rdfSingle(schema.birthDate)
+    deathDate = rdfSingle(schema.deathDate)
+
+
+def main(loadData=None):
+
+    if loadData:
+        with open(loadData, 'r', encoding='utf-8') as infile:
+            DATA = json.load(infile)
+    else:
+        DATA = {
+            'portrets': {},
+            'metadata': {
+                'date': datetime.datetime.now().isoformat()
+            }
         }
-    }
 
-    # fetch all urls to scrape
-    urls = fetchUrls(url=PORTRETURL)
+        # fetch all urls to scrape
+        urls = fetchUrls(url=PORTRETURL)
 
-    # fetch from individual pages
-    for n, url in enumerate(urls):
-        print(f"{n}/{len(urls)}\tFetching {url}")
-        pageData = fetchPortretPage(url)
+        # fetch from individual pages
+        for n, url in enumerate(urls):
+            print(f"{n}/{len(urls)}\tFetching {url}")
+            pageData = fetchPortretPage(url)
 
-        DATA['portrets'][url] = pageData
+            DATA['portrets'][url] = pageData
 
-    # dump file
-    with open('data/data.json', 'w', encoding='utf-8') as outfile:
-        json.dump(DATA, outfile, indent=4)
+        # dump file
+        with open('data/data.json', 'w', encoding='utf-8') as outfile:
+            json.dump(DATA, outfile, indent=4)
+
+    #######
+    # RDF #
+    #######
+
+    toRDF(DATA)
 
 
 def fetchUrls(url):
@@ -88,6 +170,8 @@ def fetchPortretPage(url, sleep=1):
     quote = soup.find('div', {'id': 'portrait-quote'})
     if quote:
         quote = quote.text.replace(u'\xa0', ' ')
+        quote = quote.replace(u'\u00a0', '')
+        quote = quote.strip()
 
     data['painter'] = painter
     data['date'] = date
@@ -102,5 +186,73 @@ def fetchPortretPage(url, sleep=1):
     return data
 
 
+def toRDF(d):
+
+    g = rdfSubject.db = Graph(identifier=ns)
+
+    for url in d['portrets']:
+        data = d['portrets'][url]
+
+        sameAs = []
+
+        if data['dbnl']['url'] and 'http' in data['dbnl']['url']:
+            _, dataBib = data['dbnl']['url'].split('?id=')
+            dataBib = URIRef("http://data.bibliotheken.nl/id/dbnla/" + dataBib)
+            sameAs.append(dataBib)
+
+        try:
+            birth, death = data['bio'].replace('-', ' –').split(' – ')
+        except:
+            print(data['bio'])
+
+        try:
+            birthPlace, birthYear = birth.rsplit(' ', 1)
+        except:
+            print(birth)
+            birthPlace, birthYear = None, None
+        try:
+            deathPlace, deathYear = death.rsplit(' ', 1)
+        except:
+            print(death)
+            deathPlace, deathYear = None, None
+
+        p = Person(
+            None,
+            name=[Literal(data['title'])],
+            sameAs=sameAs,
+            birthPlace=Place(BNode(birthPlace), name=[birthPlace])
+            if birthPlace else None,
+            birthDate=Literal(birthYear, datatype=XSD.gYear, normalize=False)
+            if birthYear else None,
+            deathPlace=Place(BNode(deathPlace), name=[deathPlace])
+            if deathPlace else None,
+            deathDate=Literal(deathYear, datatype=XSD.gYear, normalize=False)
+            if deathYear else None)
+        page = CreativeWork(URIRef(url))
+
+        if data['article']['name']:
+
+            name, author = data['article']['name'].rsplit(' door ', 1)
+
+            author = Person(BNode(author.replace(' ', '')), name=[author])
+
+            article = ScholarlyArticle(URIRef(data['article']['url']),
+                                       name=[name],
+                                       author=[author],
+                                       about=p)
+
+        if data['painter']:
+            painter = Person(BNode(data['painter'].replace(' ', '')),
+                             name=[data['painter']])
+
+            artwork = VisualArtwork(None, artist=painter, about=p)
+
+        page.mainEntity = p
+        p.mainEntityOfPage = page
+
+    g.bind('schema', schema)
+    g.serialize('data/data.trig', format='trig')
+
+
 if __name__ == "__main__":
-    print(main())
+    print(main(loadData='data/data.json'))
