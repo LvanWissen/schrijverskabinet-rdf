7d18b13565e4a2187f14a5542923d594a6a753cf
Leon van Wissen <leon@vwissen.nl>
2020-02-26 23:14:12 +0100

    Update for virtuoso

--- main.py
+++ main.py
@@ -2,21 +2,35 @@ import time
 import datetime
 import json
 import re
+from itertools import count
 
 import requests
 from bs4 import BeautifulSoup
 
-from rdflib import Graph, URIRef, Literal, XSD, Namespace, RDFS, BNode, OWL
+import rdflib
+from rdflib import Dataset, ConjunctiveGraph, Graph, URIRef, Literal, XSD, Namespace, RDFS, BNode, OWL
 from rdfalchemy import rdfSubject, rdfMultiple, rdfSingle
 
 PORTRETURL = "http://www.schrijverskabinet.nl/schrijverskabinet/"
 
+create = Namespace("https://vondel.humanities.uva.nl/")
 schema = Namespace("https://schema.org/")
 bio = Namespace("http://purl.org/vocab/bio/0.1/")
 foaf = Namespace("http://xmlns.com/foaf/0.1/")
+void = Namespace("http://rdfs.org/ns/void#")
+dcterms = Namespace("http://purl.org/dc/terms/")
 
-ns = Namespace(
-    "https://data.create.humanities.uva.nl/id/datasets/schrijverskabinet/")
+rdflib.graph.DATASET_DEFAULT_GRAPH_ID = create
+
+ns = Namespace("https://vondel.humanities.uva.nl/id/schrijverskabinet/")
+
+nsPerson = Namespace(
+    "https://vondel.humanities.uva.nl/id/schrijverskabinet/Person/")
+personCounter = count(1)
+
+nsArtwork = Namespace(
+    "https://vondel.humanities.uva.nl/id/schrijverskabinet/Artwork/")
+artworkCounter = count(1)
 
 
 class Entity(rdfSubject):
@@ -31,6 +45,9 @@ class Entity(rdfSubject):
     disambiguatingDescription = rdfSingle(schema.disambiguatingDescription)
 
     depiction = rdfSingle(foaf.depiction)
+    subjectOf = rdfMultiple(schema.subjectOf)
+    about = rdfSingle(schema.about)
+    url = rdfSingle(schema.url)
 
 
 class CreativeWork(Entity):
@@ -40,7 +57,36 @@ class CreativeWork(Entity):
     author = rdfMultiple(schema.author)
 
     mainEntity = rdfSingle(schema.mainEntity)
-    about = rdfSingle(schema.about)
+
+
+class DatasetClass(Entity):
+
+    # db = ConjunctiveGraph
+
+    rdf_type = void.Dataset, schema.Dataset
+
+    title = rdfMultiple(dcterms.title)
+    description = rdfMultiple(dcterms.description)
+    creator = rdfMultiple(dcterms.creator)
+    publisher = rdfMultiple(dcterms.publisher)
+    contributor = rdfMultiple(dcterms.contributor)
+    source = rdfSingle(dcterms.source)
+    date = rdfSingle(dcterms.date)
+    created = rdfSingle(dcterms.created)
+    issued = rdfSingle(dcterms.issued)
+    modified = rdfSingle(dcterms.modified)
+
+    exampleResource = rdfSingle(void.exampleResource)
+    vocabulary = rdfMultiple(void.vocabulary)
+    triples = rdfSingle(void.triples)
+
+    distribution = rdfSingle(schema.distribution)
+
+
+class DataDownload(Entity):
+    rdf_type = schema.DataDownload
+
+    contentUrl = rdfSingle(schema.contentUrl)
 
 
 class ScholarlyArticle(CreativeWork):
@@ -100,12 +146,12 @@ def main(loadData=None):
         }
 
         # fetch all urls to scrape
-        urls = fetchUrls(url=PORTRETURL)
+        pages = fetchUrls(url=PORTRETURL)
 
         # fetch from individual pages
-        for n, url in enumerate(urls):
-            print(f"{n}/{len(urls)}\tFetching {url}")
-            pageData = fetchPortretPage(url)
+        for n, (url, img) in enumerate(pages, 1):
+            print(f"{n}/{len(pages)}\tFetching {url}")
+            pageData = fetchPortretPage(url, img)
 
             DATA['portrets'][url] = pageData
 
@@ -124,13 +170,26 @@ def fetchUrls(url):
 
     r = requests.get(url)
 
-    urls = re.findall(r'(http:\/\/www\.schrijverskabinet\.nl\/portret\/.*?)"',
-                      r.text)
+    # urls = re.findall(r'(http:\/\/www\.schrijverskabinet\.nl\/portret\/.*?)"',
+    #                   r.text)
 
-    return urls
+    soup = BeautifulSoup(r.text, 'html.parser')
+
+    portraits = soup.findAll('a', class_='portrait')
+
+    pagedata = []
+    for portrait in portraits:
+        url = portrait['href']
+        el_img = portrait.find('img', recursive=False)
+
+        img = el_img.get('data-lazy-src', el_img['src'])
+
+        pagedata.append((url, img))
+
+    return pagedata
 
 
-def fetchPortretPage(url, sleep=1):
+def fetchPortretPage(url, img, sleep=1):
 
     r = requests.get(url)
     soup = BeautifulSoup(r.text, 'html.parser')
@@ -178,11 +237,16 @@ def fetchPortretPage(url, sleep=1):
         quote = quote.replace(u'\u00a0', '')
         quote = quote.strip()
 
-    depiction = soup.find('img')['src']
-    if 'portrait-no-image' in depiction:
-        depiction = None
+    if 'portrait-no-image-overview' in img or 'Vrouw01' in img:
+        img = None
     else:
-        depiction = URIRef(depiction)
+        img = URIRef(img)
+
+    artdepiction = soup.find('img')['src']
+    if 'portrait-no-image' in artdepiction or 'Vrouw01' in artdepiction:
+        artdepiction = None
+    else:
+        artdepiction = URIRef(artdepiction)
 
     data['painter'] = painter
     data['date'] = date
@@ -190,7 +254,8 @@ def fetchPortretPage(url, sleep=1):
     data['article'] = article
     data['dbnl'] = dbnl
     data['quote'] = quote
-    data['depiction'] = depiction
+    data['depiction'] = img
+    data['artdepiction'] = artdepiction
 
     if sleep:
         time.sleep(sleep)
@@ -198,9 +263,24 @@ def fetchPortretPage(url, sleep=1):
     return data
 
 
+def person2uri(name, data):
+
+    uri = data.get(name, None)
+    if uri:
+        return uri, data
+    else:
+        data[name] = nsPerson.term(str(next(personCounter)))
+
+        return data[name], data
+
+
 def toRDF(d):
 
-    g = rdfSubject.db = Graph(identifier=ns)
+    ds = Dataset()
+
+    g = rdfSubject.db = ds.graph(identifier=ns)
+
+    persondata = dict()
 
     for url in d['portrets']:
         data = d['portrets'][url]
@@ -228,17 +308,23 @@ def toRDF(d):
             print(death)
             deathPlace, deathYear = None, None
 
+        subjectOf = []
+        puri, persondata = person2uri(data['title'], persondata)
         p = Person(
-            None,
+            puri,
             name=[Literal(data['title'])],
             sameAs=sameAs,
-            birthPlace=Place(BNode("".join(
-                [i for i in birthPlace if i in 'abcdefghijklmnopqrstuvwxyz'])),
+            birthPlace=Place(BNode("".join([
+                i for i in birthPlace
+                if i.lower() in 'abcdefghijklmnopqrstuvwxyz'
+            ])),
                              name=[birthPlace]) if birthPlace else None,
             birthDate=Literal(birthYear, datatype=XSD.gYear, normalize=False)
             if birthYear else None,
-            deathPlace=Place(BNode("".join(
-                [i for i in deathPlace if i in 'abcdefghijklmnopqrstuvwxyz'])),
+            deathPlace=Place(BNode("".join([
+                i for i in deathPlace
+                if i.lower() in 'abcdefghijklmnopqrstuvwxyz'
+            ])),
                              name=[deathPlace]) if deathPlace else None,
             deathDate=Literal(deathYear, datatype=XSD.gYear, normalize=False)
             if deathYear else None,
@@ -250,28 +336,89 @@ def toRDF(d):
 
             name, author = data['article']['name'].rsplit(' door ', 1)
 
-            author = Person(BNode("".join(
-                [i for i in author if i in 'abcdefghijklmnopqrstuvwxyz'])),
-                            name=[author])
+            authoruri, persondata = person2uri(author, persondata)
+            author = Person(authoruri, name=[author])
 
             article = ScholarlyArticle(URIRef(data['article']['url']),
                                        name=[name],
                                        author=[author],
                                        about=p)
+            subjectOf.append(article)
 
         if data['painter']:
-            painter = Person(BNode("".join([
-                i for i in data['painter'] if i in 'abcdefghijklmnopqrstuvwxyz'
-            ])),
-                             name=[data['painter']])
+            painteruri, persondata = person2uri(data['painter'], persondata)
+            painter = Person(painteruri, name=[data['painter']])
 
-            artwork = VisualArtwork(None, artist=painter, about=p)
+            artwork = VisualArtwork(
+                nsArtwork.term(str(next(artworkCounter))),
+                artist=painter,
+                about=p,
+                name=[Literal(f"Portret van {data['title']}", lang='nl')],
+                depiction=data['artdepiction'])
+            subjectOf.append(artwork)
 
+            if data['origin']['url']:
+                artwork.sameAs = [URIRef(data['origin']['url'])]
+
+        p.subjectOf = subjectOf
         page.mainEntity = p
         p.mainEntityOfPage = page
 
-    g.bind('schema', schema)
-    g.serialize('data/data.trig', format='trig')
+    ########
+    # Meta #
+    ########
+
+    rdfSubject.db = ds
+
+    description = """Schrijverskabinet.nl is ontwikkeld om op een toegankelijke manier de Nederlandse literaire cultuur van de vroegmoderne tijd voor het voetlicht te brengen. De website is ingericht rondom een achttiende-eeuwse verzameling auteursportretten: het Panpoëticon Batavûm. Naast een geschiedenis van deze collectie, presenteert schrijverskabinet.nl een overzicht van de dichters en dichteressen van wie het portret in de verzameling was opgenomen. Veel van de oorspronkelijke Pan-portretten zijn verloren gegaan of (vooralsnog) onvindbaar. In die gevallen is gekozen om de auteurs waar mogelijk te tonen aan de hand van gravures, schilderijen of prenten. In de focus op het Panpoëticon schuilt de beperking van dit project. Deze website presenteert geen overzicht van alle Nederlandse dichters en dichteressen uit de vroegmoderne tijd, maar bevat uitsluitend de auteurs die in het kabinet waren opgenomen.
+
+Naast een overzicht van de dichters en dichteressen die in het Panpoëticon waren te vinden, biedt schrijverskabinet.nl een computervisualisatie van het verloren houten kabinet. Het gaat om een hypothetische reconstructie.
+
+In de sectie ‘Uit de kast’ introduceren diverse specialisten veel van de auteurs. Deze sectie is geen afgesloten geheel, maar kan in de loop van de tijd verder groeien. De korte artikels streven geen uitputtend bio- of bibliografisch doel na. Ze zijn bedoeld om de (veelal vergeten) auteurs opnieuw onder de aandacht te brengen. Achter elke bijdrage wordt verwezen naar een of twee, zo mogelijk recente en online beschikbare, studies over de besproken auteur en/of (een aspect van) zijn of haar werk, die geschikt zijn voor een nadere kennismaking en een indruk geven van de huidige stand van het onderzoek. De meest voor de handliggende secundaire bronnen (literatuurgeschiedenissen, biografische woordenboeken, wikipagina’s) noemen we in principe niet, behalve als andere publicaties (nog) ontbreken. Alleen in het geval van enkele `groten’ (Vondel, Hooft, Huygens) wijken we van onze regel af: gezien de grote hoeveelheid vaak specialistische publicaties over deze auteurs beperken we ons daar tot het noemen van de meest gebruikte bronnenuitgaven. Bij alle auteurs biedt de link naar de dbnl toegang tot meer complete bibliografische informatie.
+
+Schrijverskabinet.nl is in aanbouw. Mocht u ontbrekende portretten weten te vinden of een nog niet besproken auteur willen introduceren in een essay, neem dan contact op met Lieke van Deinsen (L.van.Deinsen@rijksmuseum.nl)."""
+
+    contributors = "Sarah Adams, Peter Altena, Pieta van Beek, Frans Blom, Roland de Bonth, Lieke van Deinsen, Feike Dietz, Michiel van Duijnen, Martine van Elk, Johanna Ferket, Josephina de Fouw, Nina Geerdink, Arie-Jan Gelderblom, Lia van Gemert, Elly Groenenboom-Draai, Anna de Haas, Ton Harmsen, Kornee van der Haven, Patrick van ‘t Hof, Rick Honings, Dirk Imhof, Jeroen Jansen, Johan Koppenol, Inger Leemans, Ad Leerintveld, Henk Looijesteijn, Geert Mak, Hubert Meeus, Marijke Meijer Drees, Sven Molenaar, Alan Moss, Nelleke Moser, Ivo Nieuwenhuis, Jan Noordegraaf, Joris Oddens, Kasper van Ommen, Timothy De Paepe, Marrigje Paijmans, Karel Porteman, Sophie Reinders, Michiel Roscam Abbing, Gijsbert Rutten, Riet Schenkeveld-van der Dussen, Nicoline van der Sijs, Jasper van der Steen, René van Stipriaan, Ton van Strien, Els Stronks, Marijke Tolsma, Ans Veltman, Ruben E. Verwaal, Arnoud Visser, Jan Waszink"
+
+    download = DataDownload(
+        None,
+        contentUrl=URIRef(
+            "https://github.com/LvanWissen/schrijverskabinet-rdf/raw/master/data/schrijverskabinet.trig"
+        ),
+        # name=Literal(),
+        url=URIRef("https://github.com/LvanWissen/schrijverskabinet-rdf"))
+
+    dataset = DatasetClass(
+        ns.term(''),
+        name=[
+            Literal("Het Schrijverskabinet - Panpoëticon Batavûm", lang='nl')
+        ],
+        about=URIRef('https://www.wikidata.org/entity/Q17319132'),
+        url=URIRef('http://www.schrijverskabinet.nl/'),
+        description=[Literal(description, lang='nl')],
+        creator=[Literal("Lieke van Deinsen"),
+                 Literal("Ton van Strien")],
+        publisher=[URIRef("https://www.leonvanwissen.nl/me")],
+        contributor=[Literal(i) for i in contributors.split(', ')],
+        source=URIRef('http://www.schrijverskabinet.nl/'),
+        date=Literal(datetime.datetime.now().isoformat(),
+                     datatype=XSD.datetime),
+        distribution=download,
+        created=None,
+        issued=None,
+        modified=None,
+        exampleResource=p,
+        vocabulary=[URIRef("https://schema.org/")],
+        triples=sum(1 for i in ds.graph(identifier=ns).subjects()))
+
+    ds.bind('owl', OWL)
+    ds.bind('dcterms', dcterms)
+    ds.bind('create', create)
+    ds.bind('schema', schema)
+    ds.bind('void', void)
+    ds.bind('foaf', foaf)
+
+    ds.serialize('data/schrijverskabinet.trig', format='trig')
 
 
 if __name__ == "__main__":
